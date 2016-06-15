aa8a16e3045ea61af50cc6306a31760cdfb84bdb
Rebecca Edwards <rebecca.edwards@gmail.com>
2016-06-15 16:43:12 +0200

    Add routes for home and forecast pages

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 var weatherForecastApp = angular.module('weatherForecastApp', ['ngRoute', 'ngResource']);
+
+weatherForecastApp.config(function($routeProvider) {
+	
+	$routeProvider
+	
+	.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'homeController'
+	})
+
+	.when('/forecast', {
+		templateUrl: 'templates/forecast.html',
+		controller: 'forecastController'
+	})
+
+});
+
+weatherForecastApp.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+weatherForecastApp.controller('forecastController', ['$scope', function($scope) {
+
+}]);
